a063128a4e30247b69f1faa636b1f5fa556bf554
Ponomareva10 <105550506+Ponomareva10@users.noreply.github.com>
2022-07-06 20:39:51 +0600

    added get

--- script.js
+++ script.js
@@ -4,19 +4,26 @@ class Car {
     carYear;
     distance;
     countOfFuel;
+    _master;
 
-    constructor(brand, model, carYear, distance, countOfFuel) {
+    constructor(brand, model, carYear, distance, countOfFuel, master) {
             this.brand = brand;
             this.model = model;
             this.carYear = carYear;
             this.distance = distance;
             this.countOfFuel = countOfFuel;
+            this._master = master;
     };
 
     getConsumptionFuel() {
         return this.countOfFuel / this.distance * 100;
     };
+
+    get master() {
+        return this._master;
+    };
 };
 
-const myCar = new Car('Toiota', 'Ipsum', '2004', 200, 20);
-console.log(myCar.getConsumptionFuel())
+const myCar = new Car('Toiota', 'Ipsum', '2004', 200, 20, 'Anna');
+console.log(myCar.getConsumptionFuel());
+console.log(myCar.master);
